1972cf580b11d0a92b457d5a3950f95d427b27ae
Aleksis Rautiainen <aleraut9@gmail.com>
2022-12-07 17:10:05 +0200

    task completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home.js';
+import About from './Components/About.js';
+import Contact from './Components/Contact.js';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1
+      style={{
+        backgroundColor: 'black',
+        color: 'white'
+      }}
+      >
+        Welcome to React Router
+      </h1>
+      <div>
+      <BrowserRouter>
+      <Link to="/">Home</Link>{' '}
+      <Link to="/about">About</Link>{' '}
+      <Link to="/contact">Contact</Link>{' '}
+      <Routes>
+        <Route exact path="/"element={<Home />} />
+        <Route path="/about"element={<About />} />
+        <Route path="/contact"element={<Contact />} />
+        <Route path="*"element={<Home />} />
+      </Routes>
+      </BrowserRouter>
+      </div>
     </div>
   );
 }
